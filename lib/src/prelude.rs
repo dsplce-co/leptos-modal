4c368c135fea963c82c6a02a539a641a0ccba514
Dmndz <barbara@dsplce.co>
2025-12-14 01:08:29 +0100

    feat: expose ModalView type

--- lib/src/prelude.rs
+++ lib/src/prelude.rs
@@ -1,2 +1,2 @@
-pub use crate::{abstracts::Modal, components::ModalCollector, use_modal};
+pub use crate::{abstracts::Modal, components::ModalCollector, components::ModalView, use_modal};
 pub use leptos_modal_macros::*;
